75559782315119b97e79bc0453a463942f994a65
Shahirul <shahiruliimalo@gmail.com>
2025-02-28 20:10:33 +0800

    feat:🌸 update landing page and add todolist page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,16 @@
 import React from "react";
-
-const Page = () => {
-  return <div>Page</div>;
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+const Home = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Taskly</h1>
+      <p className="text-gray-500">A simple task management app</p>
+      <Link href="/todolist">
+        <Button className="mt-4">Get Started</Button>
+      </Link>
+    </div>
+  );
 };
 
-export default Page;
+export default Home;
